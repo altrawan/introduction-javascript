ca03c192978b69825e666e53e5a5e14a3abaf53f
altrawan <muhammadalifputra8888@gmail.com>
2022-03-11 14:31:47 +0700

    update task

--- part-2/task2.js
+++ part-2/task2.js
@@ -1,11 +1,11 @@
-const name = [
-    'Abigail', 'Alexandra', 'Alison',
-    'Amanda', 'Angela', 'Bella',
-    'Carol', 'Caroline', 'Carolyn',
-    'Deirdre', 'Diana', 'Elizabeth',
-    'Ella', 'Faith', 'Olivia', 'Penelope'];
-
 const searchName = (key, limit, cb) => {
+    const name = [
+        'Abigail', 'Alexandra', 'Alison',
+        'Amanda', 'Angela', 'Bella',
+        'Carol', 'Caroline', 'Carolyn',
+        'Deirdre', 'Diana', 'Elizabeth',
+        'Ella', 'Faith', 'Olivia', 'Penelope'];
+
     if (typeof key !== 'string' || typeof limit !== 'number' || !cb || limit < 1) {
         return 'Pastikan semua data sudah sesuai dan limit tidak boleh kurang dari 1';
     }
@@ -15,8 +15,15 @@ const searchName = (key, limit, cb) => {
 }
 
 // untuk menentukan limit / batasan jumlah element array
-const limitName = (filter, limit) => result = filter.slice(0, limit);
+const limitName = (filter, limit) => {
+    if (filter.length === 0) {
+        return 'Nilai tidak ditemukan';
+    } else {
+        return filter.slice(0, limit);
+    }
+}
 
-// console.log(searchName('la', 3));
-// console.log(searchName(['la'], 3, limitName));
-// console.log(searchName('an', 3, limitName));
+console.log(searchName('la', 3));
+console.log(searchName(['la'], 3, limitName));
+console.log(searchName('xx', 3, limitName));
+console.log(searchName('an', 3, limitName));
--- part-2/task3.js
+++ part-2/task3.js
@@ -10,6 +10,8 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
         return 'Nilai akhir harus lebih besar dari nilai awal';
     } else if (dataArray.length < 6) { // validasi jika element array kurang dari 5
         return 'Jumlah angka dalam dataArray harus lebih dari 5';
+    } else if (nilaiAwal === nilaiAkhir) { // validasi jika nilaiAwal sama dengan nilaiAkhir
+        return 'nilai awal tidak boleh sama dengan nilai akhir'
     } else {
         // untuk menyaring element array jika element lebih besar atau sama dengan nilaiAwal dan element kurang dari atau sama dengan nilaiAkhir
         const hasil = dataArray.filter(e => e >= nilaiAwal && e <= nilaiAkhir);
@@ -20,13 +22,14 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
     }
 }
 
-// console.log(seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]));
-// console.log(seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]));
-// console.log(seleksiNilai(5, 17 , [2, 25, 4]));
-// console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]));
-// console.log(seleksiNilai(5, 20, [true, undefined, "16", 1, null]));
-// console.log(seleksiNilai("5", null, [1, 2, 4, 5]));
-// console.log(seleksiNilai(5, 20, {
-//     num1: 1,
-//     num2: 2
-// }));
+console.log(seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]));
+console.log(seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]));
+console.log(seleksiNilai(5, 17 , [2, 25, 4]));
+console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]));
+console.log(seleksiNilai(5, 20, [true, undefined, "16", 1, null]));
+console.log(seleksiNilai("5", null, [1, 2, 4, 5]));
+console.log(seleksiNilai(5, 5, [2, 3, 4, 5, 6, 7]));
+console.log(seleksiNilai(5, 20, {
+    num1: 1,
+    num2: 2
+}));
