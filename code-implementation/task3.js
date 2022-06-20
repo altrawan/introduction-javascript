9a7a7bb20b409d7f12854ca44624163455650a90
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-20 10:00:44 +0700

    Make validation max save integer

--- code-implementation/task3.js
+++ code-implementation/task3.js
@@ -1,37 +1,44 @@
 const divideAndSort = (num) => {
-    const status = num.toString().split('').includes('0');
-    let split = num.toString().split('0');
-    let result = '';
+  const status = num.toString().split('').includes('0');
+  let split = num.toString().split('0');
+  let result = '';
 
-    switch (typeof num) {
-        case 'number':
-            if (!status) {
-                console.log(`deret angka (${num}) tidak dapat dirutkan karena tidak terdapat angka 0`)
-            } else {
-                split.map(e => {
-                    let x = e.split('').sort().join('');
-                    result += x;
-                });
-                console.log(`Output : ${parseInt(result)}`);
-            }
-            break;
-        case 'bigint' :
-            if (!status) {
-                console.log(`deret angka (${num}) tidak dapat dirutkan karena tidak terdapat angka 0`)
-            } else {
-                split.map(e => {
-                    let x = e.split('').sort().join('');
-                    result += x;
-                });
-                console.log(`Output : ${BigInt(result)}`);
-            }
-            break;
-        default :
-            console.log("deret angka harus bertipe Number atau BigInt");
-            break;
-    }
-}
-  
+  switch (typeof num) {
+    case 'number':
+      if (num < Number.MAX_SAVE_INTEGER) {
+        if (!status) {
+          console.log(
+            `deret angka (${num}) tidak dapat dirutkan karena tidak terdapat angka 0`
+          );
+        } else {
+          split.map((e) => {
+            let x = e.split('').sort().join('');
+            result += x;
+          });
+          console.log(`Output : ${parseInt(result)}`);
+        }
+        break;
+      }
+
+    case 'bigint':
+      if (!status) {
+        console.log(
+          `deret angka (${num}) tidak dapat dirutkan karena tidak terdapat angka 0`
+        );
+      } else {
+        split.map((e) => {
+          let x = e.split('').sort().join('');
+          result += x;
+        });
+        console.log(`Output : ${BigInt(result)}`);
+      }
+      break;
+    default:
+      console.log('deret angka harus bertipe Number atau BigInt');
+      break;
+  }
+};
+
 divideAndSort(9007199254740991);
 // add n in last parameter, to change variable to bigint type
 divideAndSort(34824023435543210574326498230432105432123055123123n);
